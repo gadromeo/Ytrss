bfc43440f14adbcca7ae900bd2d77460f731b607
Li Zhang <lii@lis-MacBook-Air.local>
2014-07-06 14:40:24 -0500

    prints youtube video data with chart

--- hot.py
+++ hot.py
@@ -1,4 +1,6 @@
 import feedparser
+import json
+import urllib
 
 hot = feedparser.parse("http://www.billboard.com/rss/charts/hot-100")
 
@@ -7,5 +9,10 @@ print "=      BILLBOARD HOT 100      ="
 print "==============================="
 print ""
 
-for x in range(0,len(hot['entries'])):
+#for x in range(0,len(hot['entries'])):
+for x in range(0,5):
+    search = hot['entries'][x]['chart_item_title']
     print hot['entries'][x]['rank_this_week']+". "+hot['entries'][x]['chart_item_title']+" - "+hot['entries'][x]['artist']
+    results = urllib.urlopen("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q="+search+"&type=video&key=AIzaSyCjTKZ7dY0oaTvbABEvPQ26nHkVaqnGtyE")
+    youjs = json.load(results)
+    print youjs['items'][0]['id']['videoId'] + ": " + youjs['items'][0]['snippet']['title']
