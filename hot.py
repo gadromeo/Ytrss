034d064aae4e7c861ad0a9ee2a2d233971b5258b
Li Zhang <lii@lis-MacBook-Air.local>
2014-07-06 15:05:46 -0500

    watch in browser

--- hot.py
+++ hot.py
@@ -1,6 +1,7 @@
 import feedparser
 import json
 import urllib
+import webbrowser
 
 hot = feedparser.parse("http://www.billboard.com/rss/charts/hot-100")
 
@@ -9,10 +10,23 @@ print "=      BILLBOARD HOT 100      ="
 print "==============================="
 print ""
 
-#for x in range(0,len(hot['entries'])):
-for x in range(0,5):
-    search = hot['entries'][x]['chart_item_title']
+for x in range(0,len(hot['entries'])):
+#for x in range(0,5):
+    #search = hot['entries'][x]['chart_item_title']
     print hot['entries'][x]['rank_this_week']+". "+hot['entries'][x]['chart_item_title']+" - "+hot['entries'][x]['artist']
+    #results = urllib.urlopen("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q="+search+"&type=video&key=AIzaSyCjTKZ7dY0oaTvbABEvPQ26nHkVaqnGtyE")
+    #youjs = json.load(results)
+    #print youjs['items'][0]['id']['videoId'] + ": " + youjs['items'][0]['snippet']['title']
+
+print "0 to quit."
+viewvid = int(raw_input('Watch video ranked #'))
+
+while viewvid>0:
+    search = hot['entries'][viewvid-1]['chart_item_title']
     results = urllib.urlopen("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q="+search+"&type=video&key=AIzaSyCjTKZ7dY0oaTvbABEvPQ26nHkVaqnGtyE")
     youjs = json.load(results)
-    print youjs['items'][0]['id']['videoId'] + ": " + youjs['items'][0]['snippet']['title']
+    print youjs['items'][0]['snippet']['title']
+    webbrowser.open("https://www.youtube.com/watch?v="+youjs['items'][0]['id']['videoId'])
+    viewvid = int(raw_input('Watch video ranked #'))
+
+#sdf
